05a7faaebfbb58d822e177eac863508a69231289
DESKTOP-C4Q78DI <hasnainrakha58@gmail.com>
2022-09-20 10:57:47 +0500

    task completion

--- App.js
+++ App.js
@@ -1,36 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TextInput,
-  Alert, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput,CheckBox,
+  Alert, TouchableOpacity} from 'react-native';
+import {FontAwesome} from '@expo/vector-icons'
+
 
 export default function App() {
   return (
     <View style={{flex:1}}>
-      <View style={{flex:0.2,alignItems:'center',justifyContent:'center'}}>
+      <View style={{flex:0.2,alignItems:'center',justifyContent:'center',backgroundColor:'white'}}>
         {/* <Text> TOP</Text> */}
-        <Image
+        {/* <Image
         style={styles.imagestyle}
         source={require('./assets/picture.png')}
-      />
+      /> */}
+      <Text
+      style={{
+          
+        fontWeight:"900",
+        fontSize:40,
+        color:'#00A19D'
+      }}>Sign Up</Text>
       </View>
-      <View style={{flex:0.6,alignItems:'center',justifyContent:'center'}}>
-      <TextInput style={{backgroundColor:'#708090',marginBottom:20, marginTop:10,width:320,color:'white'}}
-      placeholder='first name'/>
-      <TextInput style={{backgroundColor:'#708090',marginBottom:20, marginTop:10,width:320,color:'white'}}
-      placeholder='last name'/>
-      <TextInput style={{backgroundColor:'#708090',marginBottom:20, marginTop:10,width:320,color:'white'}}
-      placeholder='email'/>
-      <TextInput style={{backgroundColor:'#708090',marginBottom:20, marginTop:10,width:320,color:'white'}}
+      <View style={{flex:0.6,alignItems:'center',marginTop:30}}>
+      <TextInput style={{backgroundColor:'#FFF5E4',marginBottom:20,width:320,height:50,color:'black',borderRadius:20,padding:5}}
+      placeholder='e-mail'/>
+      <TextInput secureTextEntry={true} style={{backgroundColor:'#FFF5E4',marginBottom:20,width:320,height:50,color:'black',borderRadius:20,padding:5}}
       placeholder='password'/>
+      <TextInput secureTextEntry={true} style={{backgroundColor:'#FFF5E4',marginBottom:20,width:320,height:50,color:'black',borderRadius:20,padding:5}}
+      placeholder='repeat password'/>
+      <Text
+        style={{
+          
+          fontWeight:"100",
+          fontSize:10
+        }}
+        >Sign up with social account</Text>
+        <View style={{flexDirection:"row",justifyContent:'center',backgroundColor:'white'}}>
+        
+        <FontAwesome name = 'google' size={25} style={{color:'blue'}}></FontAwesome>
+        <FontAwesome name = 'facebook-square' size={25} style={{color:'blue',marginLeft:10}} ></FontAwesome>
+        </View>
+  
       
       </View>
+      
       <View style={{flex:0.2,alignItems:'center',justifyContent:'center'}}>
       <TouchableOpacity
         style={ {
-          backgroundColor:"#ffa500",
+          backgroundColor:"#009DAE",
           padding: 10,
           width:320,
+        
           alignItems:'center',
-          justifyContent:'center'
+          justifyContent:'center',
+          borderRadius:20
 
 
         }}
@@ -42,8 +65,17 @@ export default function App() {
           fontWeight:"600",
           fontSize:20
         }}
-        >Enter</Text>
+        >Sign Up</Text>
       </TouchableOpacity>
+      <Text
+        style={{
+          
+          fontWeight:"600",
+          marginTop:15,
+          fontSize:15,
+          color:"#009DAE"
+        }}
+        >Read User License Agreement</Text>
       </View>
 
       {/* <Image
